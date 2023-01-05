1e908bf7711a64f879593b0ffff082154adda1e6
KeyaruUzumaki <92820887+KeyaruUzumaki@users.noreply.github.com>
2023-01-05 13:29:30 +0200

    pagination

--- shop/views.py
+++ shop/views.py
@@ -37,6 +37,7 @@ class Shop(ListView):
     model = Product
     template_name = 'shop/shop.html'
     context_object_name = 'products_list'
+    paginate_by = 9
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
